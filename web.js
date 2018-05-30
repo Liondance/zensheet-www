fa6d8ff60c879d8391da73194658ddb28c669bf4
prmm95 <prmm95@gmail.com>
2018-05-29 23:00:47 -0400

    feat: ZenSheet site redirects to authentication middleware
    Now the ZenSheet site provides to links to signup or sign in with
    the AWS Cognito Authentication Platform. The information that is
    needed for this is fetched from environment variables that must
    be defined on the host computer. Further documentation of this
    procedure will be added to the documentation of this repository.

--- web.js
+++ web.js
@@ -2,20 +2,41 @@
 // web.js
 //
 
+// Requirements
 var
     express = require('express')
   , fs      = require('fs')
   , http    = require('http')
   , https   = require('https');
 
+// Configuration
 var app = express();
 
-app.use(express.static('views'));
+// Environment variables
+var config = require('./config')
+
+// Setting up the authentication link
+const domain_name = "https://" + config.COGNITO_DOMAIN + '/';
+const response_type = 'response_type=' + 'code' + '&';
+const redirect_uri = 'redirect_uri=' + config.COGNITO_APP_CLIENT_CALLBACK_URL + '&' 
+const client_id = 'client_id=' + config.COGNITO_APP_CLIENT_ID
+const authentication_parameters = '?' + response_type + redirect_uri + client_id;
+
+ + 'login?' +  +  +  + 
+
+
+
+
+
+
 
 app.set('port', process.env.PORT || 8080);
 
+app.use(express.static('views'));
+
 app.set('view engine', 'ejs');
 
+// Routes
 app.get('/', function(req, res) {
   res.render('index');
 });
@@ -40,6 +61,15 @@ app.get('/contact', function (req, res) {
   res.render('contact');
 });
 
+app.get('/signup', function (req, res) {
+  res.redirect(domain_name + 'signup' + authentication_parameters);
+});
+
+app.get('/signin', function (req, res) {
+  res.redirect(domain_name + 'login' + authentication_parameters);
+});
+
+// Starting the server
 http.createServer(app).listen(app.get('port'), function() {
   console.log("Listening on " + app.get('port'));
 });
